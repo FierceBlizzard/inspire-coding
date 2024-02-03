e584f9a86401e367abc01d49e3d2431e56fbb40d
Iclyn Taero <43586417+FierceBlizzard@users.noreply.github.com>
2024-02-02 19:10:08 -0500

    pushing changes

--- coding/src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ coding/src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "coding19850fc2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
